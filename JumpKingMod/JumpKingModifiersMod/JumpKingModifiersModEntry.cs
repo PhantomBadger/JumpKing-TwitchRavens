10bb68e3a42784d3f8ef36cbc43974b706726cee
Josh Leland <josh@joslot.com>
2023-03-08 21:31:41 +0000

    Added stub for Modifiers project

---
+++ JumpKingMod/JumpKingModifiersMod/JumpKingModifiersModEntry.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Logging;
+using Logging.API;
+using PBJKModBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKingModifiersMod
+{
+    [JumpKingMod("Jump King Modifiers", "Init")]
+    public class JumpKingModifiersModEntry
+    {
+        public static ILogger Logger;
+
+        /// <summary>
+        /// Entry point for our mod, initiates all of the patching
+        /// </summary>
+        public static void Init()
+        {
+            Logger = ConsoleLogger.Instance;
+            try
+            {
+                var harmony = new Harmony("com.phantombadger.jumpkingmodifiersmod");
+                harmony.PatchAll();
+
+                Logger.Information($"====================================");
+                Logger.Information($"Jump King Modifiers Pre-Release!");
+                Logger.Information($"====================================");
+
+
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"Error on Modifiers Init {e.ToString()}");
+            }
+            Logger.Information("Modifiers Init Called!");
+        }
+    }
+}
---
+++ JumpKingMod/JumpKingModifiersMod/Patching/PlayerValuesManualPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using PBJKModBase.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKingModifiersMod.Patching
+{
+    public class PlayerValuesManualPatch : IManualPatch
+    {
+        private const float DefaultSpeed = 7f;
+        private const float DefaultBounce = 0.5f;
+        private const float DefaultJumpTime = 0.6f;
+
+        public void SetUpManualPatch(Harmony harmony)
+        {
+            // TODO: patch static getters for player values
+        }
+    }
+}
