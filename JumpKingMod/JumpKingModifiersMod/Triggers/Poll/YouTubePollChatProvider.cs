44986afd94ba6fd22c4a1c171148ffce0f61c1cd
Josh Leland <josh@joslot.com>
2023-07-17 22:09:27 +0100

    Minor progress on YouTube Chat Provider

--- JumpKingMod/JumpKingModifiersMod/Triggers/Poll/YouTubePollChatProvider.cs
+++ JumpKingMod/JumpKingModifiersMod/Triggers/Poll/YouTubePollChatProvider.cs
@@ -1,5 +1,8 @@
 ﻿using JumpKingModifiersMod.API;
+using Logging.API;
+using PBJKModBase.YouTube;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +14,24 @@ namespace JumpKingModifiersMod.Triggers.Poll
     {
         public event ChatVoteDelegate OnChatVote;
 
-        public void ClearPerPollData()
+        private readonly ILogger logger;
+        private readonly YouTubeChatClient youtubeClient;
+        private readonly ConcurrentDictionary<string, byte> alreadyVotedChatters;
+
+        private bool isEnabled;
+
+        public YouTubePollChatProvider(YouTubeChatClient youtubeClient, ILogger logger)
+        {
+            this.youtubeClient = youtubeClient ?? throw new ArgumentNullException(nameof(youtubeClient));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            alreadyVotedChatters = new ConcurrentDictionary<string, byte>();
+            isEnabled = false;
+
+            youtubeClient.OnMessageBatchReceived += OnMessageReceived;
+        }
+
+        public void Dispose()
         {
             throw new NotImplementedException();
         }
@@ -21,14 +41,49 @@ namespace JumpKingModifiersMod.Triggers.Poll
             throw new NotImplementedException();
         }
 
-        public void Dispose()
+        public bool EnableChatProvider()
         {
             throw new NotImplementedException();
         }
 
-        public bool EnableChatProvider()
+        public void ClearPerPollData()
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Called by the underlying Twitch Client when a message is received, updates the UI Entity
+        /// </summary>
+        private void OnMessageReceived(object sender, YouTubeChatMessageBatchArgs chatMessages)
+        {
+            try
+            {
+                if (!isEnabled || chatMessages == null || chatMessages.LiveChatMessages == null)
+                {
+                    return;
+                }
+
+                // TODO
+                //// If they have already voted, then dip (unless it's me because im special hehe)
+                //if (alreadyVotedChatters.ContainsKey(e.ChatMessage.UserId) &&
+                //    !e.ChatMessage.DisplayName.Equals("PhantomBadger", StringComparison.OrdinalIgnoreCase))
+                //{
+                //    return;
+                //}
+
+                //// Get the choice number from the message
+                //if (!TryGetPollChoiceNumberFromMessage(e.ChatMessage.Message, out int choiceNumber))
+                //{
+                //    return;
+                //}
+
+                //alreadyVotedChatters.TryAdd(e.ChatMessage.UserId, 0);
+                //OnChatVote?.Invoke(e.ChatMessage.DisplayName, choiceNumber);
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Encountered Exception during OnMessageReceived: {ex.ToString()}");
+            }
         }
     }
 }
